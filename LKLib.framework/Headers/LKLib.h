0492b9cf5347a2d4294bc5dcf2a32f5f0b34b13e
JohnatasRBCS <johnatas14@gmail.com>
2019-10-14 12:39:31 -0300

    Add files via upload

---
+++ LKLib.framework/Headers/LKLib.h
@@ -0,0 +1,22 @@
+//
+//  LKBLECommunication.h
+//  LKBLECommunication
+//
+//  Created by Daniel Sandoval on 23/10/16.
+//  Copyright © 2016 Loop CE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LKBLECommunication.
+FOUNDATION_EXPORT double LKLibVersionNumber;
+
+//! Project version string for LKBLECommunication.
+FOUNDATION_EXPORT const unsigned char LKLibString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LKBLECommunication/PublicHeader.h>
+
+FOUNDATION_EXPORT NSString *const kLKBLEDisabledNotification;
+FOUNDATION_EXPORT NSString *const kLKBLEEnabledNotification;
+FOUNDATION_EXPORT NSString *const kLKBLEUnsupportedNotification;
+FOUNDATION_EXPORT NSString *const kLKDoorUnlockNotification;
